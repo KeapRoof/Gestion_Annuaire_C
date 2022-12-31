fc17ed1545910099ad340aea53975878370b0dab
Clément <clement.le-goaster@etu.univ-lyon1.fr>
2022-12-31 17:26:01 +0100

    première interface pour l'API

--- main.c
+++ main.c
@@ -15,7 +15,32 @@
 //fichier .h & .c séparés
 
 int main(void) {
-    ajouter_client("annuaire.csv", "Dupont", "Jean", "75000", "Paris", "0123456789","jean.dupont@gmail.com", "Ingenieur");
-    supprimer_client("annuaire.csv", "jean.dupont@gmail.com");
-    modifier_mel_client("annuaire.csv", "richard.rey-du-letellier@server.fr", "richard.rey-du-letellier@server.fr");
+    int choix;
+    printf("--------------------------------------------------\n");
+    printf("1. Ajouter un nouveau client\n");
+    printf("2. Modifier le mel d'un client\n");
+    printf("3. Modifier autres elements que le mel d'un client\n");
+    printf("4. Supprimer un client\n");
+    printf("5. Filtrer les clients avec des données manquantes\n");
+    printf("6. Filtrer un champ\n");
+    printf("7. Filtrer en combinant deux champs\n");
+    printf("--------------------------------------------------\n");
+    printf("-> ");
+    scanf("%d", &choix);
+    switch (choix)
+    {
+    case 1:
+        ajouter_client("annuaire.csv", "Dupont", "Jean", "75000", "Paris", "0123456789","jean.dupont@gmail.com", "Ingenieur");
+        break;
+    case 2:
+        modifier_mel_client("annuaire.csv", "richard.rey-du-letellier@server.fr", "richard.rey-du-letellier@server.fr");
+        break;
+    case 4:
+        supprimer_client("annuaire.csv", "jean.dupont@gmail.com");
+        break;
+    default:
+        printf("Erreur lors de la saisie OU fonctionnalité non disponible pour le moment\n");
+        return -1; // Erreur lors de la saisie 
+    }
+    return 0; // Retour normal
 }
